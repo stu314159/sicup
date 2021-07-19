d76952518003b576241a40811efc13bb382d9098
Stu Blair <stu314159@gmail.com>
2021-07-19 15:24:12 +0000

    created do-nothing application to check functionality of CMakeLists.txt and build system

---
+++ simple_channel.cpp
@@ -0,0 +1,16 @@
+#include "palabos2D.h"
+#include "palabos2D.hh"
+
+using namespace plb;
+using namespace plb::descriptors;
+using namespace std;
+
+typedef double T;
+#define DESCRIPTOR D3Q19Descriptor
+
+int main(int argc, char* argv[])
+{
+	plbInit(&argc,&argv);
+
+	return 0;
+}
