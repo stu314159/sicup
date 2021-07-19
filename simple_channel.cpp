4072076405871d0c70ebe023882f39c234cdf609
Stu Blair <stu314159@gmail.com>
2021-07-19 15:36:55 +0000

    incremental changes - not yet ready to run real simulation

--- simple_channel.cpp
+++ simple_channel.cpp
@@ -6,11 +6,35 @@ using namespace plb::descriptors;
 using namespace std;
 
 typedef double T;
-#define DESCRIPTOR D3Q19Descriptor
+#define DESCRIPTOR descriptors::D3Q19Descriptor
 
 int main(int argc, char* argv[])
 {
 	plbInit(&argc,&argv);
+	plint numCores = global::mpi().getSize();
+	pcout << "Number of MPI processes: " << numCores << std::endl;
+
+	plint N;
+	try {
+		global::argv(1).read(N);
+	}
+	catch(...)
+	{
+		pcout << "Wrong parameters. The syntax is " << std::endl;
+		pcout << argv[0] << " N" << std::endl;
+		pcout << "where N is the resolution of the mesh " << std::endl;
+		exit(1);
+	}
+
+	// create incompressible flow parameters object
+	IncomprFlowParam<T> parameters(
+			(T) 1e-2, // uMax
+			(T) 100., // Re
+			N,        // N (read from input
+			1.,      // lx
+			1.,      // ly
+			5.       // lz
+	);
 
 	return 0;
 }
